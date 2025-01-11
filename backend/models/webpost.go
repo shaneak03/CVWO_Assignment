6c0f9106f4bc407a009d1cac43d31543d52f198a
shaneak03 <shaneak03@gmail.com>
2025-01-11 20:36:43 +0800

    undo backend changes

--- backend/models/webpost.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type WebPost struct {
-	ID      int    `json:"id"`
-	Author  string `json:"author"`
-	Title   string `json:"title"`
-	Date    string `json:"date"`
-	Content string `json:"content"`
-}
